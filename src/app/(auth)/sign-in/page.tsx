d3179043e1c5c65311d64fc31e89c07aec1b1b51
Wattana Virunhateerasan <watku_1967@hotmail.com>
2026-05-22 14:47:40 +0700

    Update Link component prefetching for front layout

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,12 @@
 // http:localhost:3000/sign-in
 // ไม่เอา (auth) มาเป็น url
+import Link from "next/link";
+
 export default function SignInPage() {
   return (
     <main>
       Sign In Page
+      <Link href="/">Home</Link>
     </main>
   );
 }
--- src/app/(front)/about/page.tsx
+++ src/app/(front)/about/page.tsx
@@ -1,8 +1,11 @@
 // http://localhost:3000/about
+import Link from "next/link";
+
 export default function AboutPage() {
   return (
     <div>
       <p>About Us</p>
+      <Link href="/">Home</Link> 
     </div>
   );
 }
